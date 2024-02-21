c31569f56acd7d26b158bf57da0ebef5c9fceb94
sanchi285 <bismoy.sanchi@gmail.com>
2024-02-21 22:48:54 +0600

    Scaffold wiget add

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,13 @@
 import "package:flutter/material.dart";
-void  main(){
-  runApp(const MaterialApp(home: Text("Hello world"),));
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello world"),
+        ),
+      ),
+    ),
+  );
 }
