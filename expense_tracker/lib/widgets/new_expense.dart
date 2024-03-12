2bd631de53b04b5d53fce09208fe8883401ba79f
sanchi285 <bismoy.sanchi@gmail.com>
2024-03-13 00:54:17 +0600

    date picker has been added

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -9,18 +9,74 @@ class NewExpense extends StatefulWidget{
 }
 
 class _NewExpenseState extends State<NewExpense>{
+  var _enteredTitle = '';
+
+  final _titleController =  TextEditingController();
+  final _amountController =  TextEditingController();
+  void _presentDatePicker(){
+
+    final now = DateTime.now();
+    final fisrtdate = DateTime(now.year-1,now.month,now.day);
+    showDatePicker(
+    context: context,
+    initialDate: now, 
+    firstDate: fisrtdate, 
+    lastDate: now);
+  }
+
+  @override
+  void dispose(){
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _saveTitleInput(String inputValue){}
+
   @override
   Widget build (BuildContext context){
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return  Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+           controller: _titleController, 
            maxLength: 50,
-           decoration: InputDecoration(
+           decoration: const InputDecoration(
             label: Text('Title')
            ),
-          )
+          ),
+          Row(
+            
+            children: [Expanded(child: TextField(
+           controller: _amountController, 
+           keyboardType: TextInputType.number,
+           maxLength: 50,
+           decoration: const InputDecoration(
+            label: Text('Amout'),
+            prefixText: "\$"
+           ),
+          )),
+          const SizedBox(width: 16,),
+          Expanded(child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+            
+            const Text('Selected Date'),
+            IconButton(onPressed: _presentDatePicker, 
+            icon: const Icon(Icons.calendar_month))
+
+          ]),)
+          ]),
+          
+          Row(children: [
+            TextButton(onPressed: (){Navigator.pop(context);}, child: const Text("Cancel")),
+            ElevatedButton(onPressed: (){
+              print(_titleController.text);
+             // print(_amountController.text);
+            }, 
+            child: const Text('Save expense'))
+          ],)
         ]
       ),
     );
